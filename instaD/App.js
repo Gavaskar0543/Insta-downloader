db9d4718ed75d27b3f5b791bcd977a47eb42c1a8
Gavaskar <gavaskaraeg@gmail.com>
2024-01-21 15:14:35 +0000

    input box create

--- instaD/App.js
+++ instaD/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import TextInput from './Components/TextInputComponent';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <TextInput/>
     </View>
   );
 }
@@ -13,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fontColor:{
+    color:'white'
+  }
 });
---
+++ instaD/Components/TextInputComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, TextInput, YellowBox} from 'react-native';
+
+const TextInputComponent = () => {
+  const [text, onChangeText] = React.useState('Enter URL');
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    backgroundColor:'yellow',
+    color:'brown',
+    fontWeight:900,
+    borderWidth: 1,
+    padding: 10,
+    fontFamily:'Poppins'
+  },
+});
+
+export default TextInputComponent;
